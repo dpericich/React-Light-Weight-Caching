93d2f52f46f7d65ee668e713cf49970bae8df55b
Daniel <dpericich@gmail.com>
2022-08-14 22:42:18 -0500

    created PizzaTable component to display restaurant information

---
+++ src/PizzaTable.js
@@ -0,0 +1,36 @@
+import { Table, Column, HeaderCell, Cell } from 'rsuite-table';
+import 'rsuite-table/dist/css/rsuite-table.css'
+
+const PizzaTable = ({data}) => {
+  console.log(data)
+  return(
+    <Table autoHeight data={data} rowHeight={50}>
+      <Column width={150} minwidth={150} fixed>
+        <HeaderCell>Name</HeaderCell>
+        <Cell dataKey="name" />
+      </Column>
+
+      <Column width={150} minwidth={150} fixed>
+        <HeaderCell>Location</HeaderCell>
+        <Cell dataKey="location" />
+      </Column>
+
+      <Column width={100} minwidth={150} align={'center'} fixed>
+        <HeaderCell>Price</HeaderCell>
+        <Cell dataKey="price" />
+      </Column>
+
+      <Column width={100} minwidth={150} fixed>
+        <HeaderCell>Open</HeaderCell>
+        <Cell dataKey="open" />
+      </Column>
+
+      <Column width={100} minwidth={150} fixed>
+        <HeaderCell>Close</HeaderCell>
+        <Cell dataKey="close" />
+      </Column>
+    </Table>
+  )
+};
+
+export default PizzaTable;
