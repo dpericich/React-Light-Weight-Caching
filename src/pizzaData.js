22cf511edcb010ad1a3f7a415c587eda11817b25
Daniel <dpericich@gmail.com>
2022-08-14 22:41:47 -0500

    created data for seeding the table and price dropdown

---
+++ src/pizzaData.js
@@ -0,0 +1,14 @@
+const pizzaData = [
+  {name: 'Papa Jose', location: 'E. 6th Road', price: '$', open: '10:00', close: '22:00'},
+  {name: 'Mama Mias', location: 'S. 12th Ave', price: '$$', open: '12:00', close: '20:00'},
+  {name: 'The Big Cheese', location: 'N. 18th Place', price: '$', open: '11:00', close: '23:00'},
+  {name: 'Nothing but Meat', location: 'W. 1st Street', price: '$$', open: '10:00', close: '20:00'},
+  {name: 'High Flying Slice', location: 'NE. 5th Ave', price: '$$$', open: '12:00', close: '22:00'},
+  {name: 'Fish Roe Pies', location: 'NW. 3rd Street', price: '$$$$', open: '10:00', close: '22:00'},
+  {name: 'Pizza Shack', location: 'S. 8th Road', price: '$', open: '10:00', close: '23:00'}
+];
+
+export const prices = ['$', '$$', '$$$', '$$$$'];
+
+
+export default pizzaData;
