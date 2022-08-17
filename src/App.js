939bae04d3c1d421d4f9b36803975727578c54ed
Daniel <dpericich@gmail.com>
2022-08-16 23:03:35 -0500

    Updated App to include table and allow for filtering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import pizzaData, { prices } from './pizzaData';
+import PizzaTable from './PizzaTable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const cacheExpirationSeconds = 900;
+const NO_PRICE = "no price selected"
+
+const App = () => {
+  const [originalRecords, setOriginalRecords] = useState([]);
+  const [displayRecords, setDisplayRecords] = useState([]);
+  const [lastHTTPCall, setLastHTTPCall] = useState(Date.now());
+
+  useEffect(() => {
+    // Simulate our inital call to the server for records
+    const serverRecords = pizzaData;
+    setOriginalRecords(serverRecords);
+    setDisplayRecords(serverRecords);
+  }, [])
+
+  const checkCacheExpiration = () => {
+    if (Date.now() > lastHTTPCall + cacheExpirationSeconds ) {
+      // Make new call to the server for updated records
+      const serverResponse = pizzaData;
+      setOriginalRecords(pizzaData);
+      setLastHTTPCall(Date.now());
+    }
+  }
+
+  const filterByPrice = (e) => {
+    let price = e.target.value;
+    checkCacheExpiration();
+
+    if (price === NO_PRICE) {
+      setDisplayRecords(originalRecords);
+      return
+    };
+
+    let newRecords = originalRecords.filter((restaurant) => restaurant.price === price);
+    setDisplayRecords(newRecords);
+  }
+
+  return(
+    <div className="container">
+      <h1>Pizza Store Directory Page</h1>
+      <div>
+        <select onChange={(e) => filterByPrice(e)}>
+          <option value={NO_PRICE}>Any Price</option>
+          { prices.map(price => <option value={price}>{price}</option>) }
+        </select>
+      </div>
+      <PizzaTable data={displayRecords}/>
     </div>
-  );
-}
+  )
+};
 
 export default App;
